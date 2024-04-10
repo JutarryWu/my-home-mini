fb020a26c6ea39535f8bf21bbe8bb8d99ae7583a
JutarryWu <wuzihao30109@163.com>
2024-04-10 23:52:29 +0800

    初始化项目

--- src/store/index.ts
+++ src/store/index.ts
@@ -6,4 +6,13 @@ export const  useUserStore = defineStore('user', {
             nickName: ''
         }
     },
+    persist: {
+        storage: {
+            getItem: uni.getStorageSync(),
+            setItem: uni.setStorageSync(),
+        }
+    }
 })
+
+// 参考这个文件
+// https://blog.csdn.net/weixin_42614447/article/details/134335447
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from "vite";
 import uni from "@dcloudio/vite-plugin-uni";
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [uni()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      'components': path.resolve(__dirname, './src/components'),
+      'api': path.resolve(__dirname, './src/api'),
+      'assets': path.resolve(__dirname, './src/assets'),
+      'styles': path.resolve(__dirname, './src/styles'),
+      'utils': path.resolve(__dirname, './src/utils'),
+      'views': path.resolve(__dirname, './src/views'),
+    },
+  },
 });
